b4be38dad52967a4512aa1d5933227bb158b503b
ShigeruOda <42311356+shigeru-oda@users.noreply.github.com>
2023-02-17 23:54:40 +0900

    Update test_handler.py

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from hello_world.app import app as hello_world_app
 from item.app import app as item_app
+import json
 
 hello_world_client = TestClient(hello_world_app)
 item_client = TestClient(item_app)
@@ -10,20 +11,24 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello SAM World"}
 
-def test_read_item():
-    response = item_client.get("/item?item_id=1")
+def test_get_item():
+    response = item_client.get("/item?id=ABC")
     assert response.status_code == 200
-    assert response.json() == {"1": {"name": "name-0001","description":"desc-0001","price":10.0}}
-    
-def test_read_item_error():
-    response = item_client.get("/item?item_id=6")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Item not found"}
-    
-def test_create_item():
+    assert response.json() == {"id": "ABC", "name": "read_item+ABC","description":"read_item+ABC","price":100.0}
+
+def test_post_item():
     response = item_client.post(
-        "/item/9",
+        "/item",
         json={"name": "name-0009","description":"desc-0009","price":90.0}
     )
+    del_id_res = response.json()
+    del del_id_res["id"]
     assert response.status_code == 201
-    assert response.json() =={"9": {"name": "name-0009","description":"desc-0009","price":90.0}}
+    assert del_id_res =={"name": "name-0009","description":"desc-0009","price":90.0}
+
+def test_put_item():
+    response = item_client.put(
+        "/item/9"
+    )
+    assert response.status_code == 201
+    assert response.json() =={"id": "9", "name": "update_item+9","description":"update_item+9","price":200.0}
